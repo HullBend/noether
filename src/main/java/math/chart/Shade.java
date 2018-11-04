9318dcbc43fa73a47d19212266ca5c08216dcc8a
HullBend <HullBend@users.noreply.github.com>
2018-11-05 00:41:53 +0100

    awt.Color wrapper

---
+++ src/main/java/math/chart/Shade.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2018 SPZ
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package math.chart;
+
+import java.awt.Color;
+
+public final class Shade {
+
+    public static final Color WHITE = Color.WHITE;
+    public static final Color LIGHT_GRAY = Color.LIGHT_GRAY;
+    public static final Color GRAY = Color.GRAY;
+    public static final Color DARK_GRAY = Color.DARK_GRAY;
+    public static final Color BLACK = Color.BLACK;
+    public static final Color RED = Color.RED;
+    public static final Color PINK = Color.PINK;
+    public static final Color ORANGE = Color.ORANGE;
+    public static final Color YELLOW = Color.YELLOW;
+    public static final Color GREEN = Color.GREEN;
+    public static final Color MAGENTA = Color.MAGENTA;
+    public static final Color CYAN = Color.CYAN;
+    public static final Color BLUE = Color.BLUE;
+
+    private Shade() {
+        throw new AssertionError();
+    }
+}
