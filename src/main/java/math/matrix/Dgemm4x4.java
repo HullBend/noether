d4ee0f6ad590817e0ab2c84b8d0357dfc193d831
HullBend <HullBend@users.noreply.github.com>
2018-12-08 18:41:31 +0100

    add copyright Dr. Michael Lehn

--- src/main/java/math/matrix/Dgemm4x4.java
+++ src/main/java/math/matrix/Dgemm4x4.java
@@ -1,8 +1,27 @@
+/*
+ * Copyright Dr. Michael Lehn, Ulm University (C algorithm)
+ * Copyright 2018 SPZ (Java translation)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package math.matrix;
 
 import java.util.Arrays;
 
 /**
+ * A straightforward Java translation of Michael Lehn's pure ANSI C variant of a
+ * cache-friendly dgemm routine.
+ * <p>
  * https://apfel.mathematik.uni-ulm.de/~lehn/sghpc/day08/page06.html
  * <p>
  * http://apfel.mathematik.uni-ulm.de/~lehn/sghpc/gemm/page02/index.html
