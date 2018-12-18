3d6b4695e61907d5204e0b986c9cd2cd3de5a50b
HullBend <HullBend@users.noreply.github.com>
2018-12-18 01:27:19 +0100

    ulmBLAS experiments

---
+++ src/main/java/math/matrix/Scal.java
@@ -0,0 +1,23 @@
+package math.matrix;
+
+//
+// Compute X *= alpha
+//
+public final class Scal {
+
+	public static void scal(int n, double alpha, double[] x, int xOff, int incX) {
+		if (alpha != 1.0 && alpha != 0.0) {
+			for (int i = 0; i < n; ++i) {
+				x[xOff + i * incX] *= alpha;
+			}
+		} else if (alpha == 0.0) {
+			for (int i = 0; i < n; ++i) {
+				x[xOff + i * incX] *= 0.0;
+			}
+		}
+	}
+
+	private Scal() {
+		throw new AssertionError();
+	}
+}
